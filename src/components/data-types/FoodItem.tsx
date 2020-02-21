7d7d74002b9030a73d8c5076402d9944eb89d3c6
James Youngblood <james@youngbloods.org>
2020-02-21 18:51:03 +0000

    add FoodItem class

---
+++ src/components/data-types/FoodItem.tsx
@@ -0,0 +1,36 @@
+export default class FoodItem {
+  private name:string;
+  private expiryDate:Date;
+  private priority:number;
+  private owner:string;
+  private note:string;
+  
+  constructor(name:string, expiryDate:Date, priority:number = 0, 
+      owner:string = null, note:string = null) {
+    this.setFields(name, expiryDate, priority, owner, note);
+  }
+  
+  setName(name:string) { this.name = name }
+  getName() { return this.name }
+  
+  setExpiryDate(date: Date) { this.expiryDate = date }
+  getExpiryDate() { return this.expiryDate }
+  
+  setPriority(priority:number) { this.priority = priority }
+  getPriority() { return this.priority }
+  
+  setOwner(owner:string) { this.owner = owner }
+  getOwner() { return this.owner }
+  
+  setNote(note:string) { this.note = note }
+  getNote() { return this.note }
+  
+  setFields(name:string, expiryDate:Date, priority:number = 0, 
+      owner:string = null, note:string = null) {
+    this.name = name;
+    this.expiryDate = expiryDate;
+    this.priority = priority;
+    this.owner = owner;
+    this.note = note;
+  }
+}
